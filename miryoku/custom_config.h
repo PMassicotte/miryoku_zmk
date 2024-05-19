33b271c442b6d2a8dd7a2d4083613b72e662e8d0
Philippe Massicotte <pmassicotte@hotmail.com>
2024-05-19 06:54:47 -0400

    try adding custom combo

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,13 +1,38 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-/ {
 
+/ {
   combos {
     compatible = "zmk,combos";
-
-    caps_lock {
-      bindings = <&caps_word>;
-      key - positions = <14 15>;
+    combo_ae {
+      timeout - ms = <50>;
+      key - positions = <3 13>;
+      bindings = <&kp DE_A_UMLAUT>;
+    };
+    combo_oe {
+      timeout - ms = <50>;
+      key - positions = <9 3>;
+      bindings = <&kp DE_O_UMLAUT>;
+    };
+    combo_ue {
+      timeout - ms = <50>;
+      key - positions = <3 7>;
+      bindings = <&kp DE_U_UMLAUT>;
+    };
+    combo_cp {
+      timeout - ms = <50>;
+      key - positions = <27 28>;
+      bindings = <&kp LC(DE_C)>;
+    };
+    combo_cut {
+      timeout - ms = <50>;
+      key - positions = <26 27>;
+      bindings = <&kp LC(DE_X)>;
+    };
+    combo_pst {
+      timeout - ms = <50>;
+      key - positions = <28 29>;
+      bindings = <&kp LC(DE_V)>;
     };
   };
 };
