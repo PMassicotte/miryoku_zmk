0a3f20766c0fb43ba28eef512de14c6702e6a8ff
Philippe Massicotte <pmassicotte@hotmail.com>
2024-05-19 06:37:49 -0400

    try adding custom combo

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,12 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
+/ {
 
+  combos {
+    compatible = "zmk,combos";
+
+    caps_lock {
+      bindings = <&caps_word>;
+      key - positions = <14 15>;
+    };
+  };
